1ad6666a6a4f1fbb157385cf23ab4665e437841a
Kamil Seweryn <sewerk@gmail.com>
2014-10-22 15:14:24 +0200

    send mail intent + ButterKnife

--- app/src/main/java/pl/srw/billcalculator/AboutActivity.java
+++ app/src/main/java/pl/srw/billcalculator/AboutActivity.java
@@ -1,16 +1,40 @@
 package pl.srw.billcalculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class AboutActivity extends Activity {
+
+    public static final String RECEIVER_EMAIL = "sewerk@gmail.com";
+    @InjectView(R.id.textView_emalme) TextView tvEmailme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.textView_emalme)
+    public void sendEmail() {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{RECEIVER_EMAIL});
+        i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
+        try {
+            startActivity(Intent.createChooser(i, getString(R.string.emailme)));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, getString(R.string.email_client_missing), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
