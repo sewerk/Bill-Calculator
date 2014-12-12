5ef93f10bb83ff6c3bb396759dc1802d035f46da
Kamil Seweryn <--global>
2014-12-12 10:57:41 +0100

    price settings for PGE G12

--- app/src/main/java/pl/srw/billcalculator/SettingsFragment.java
+++ app/src/main/java/pl/srw/billcalculator/SettingsFragment.java
@@ -3,11 +3,11 @@ package pl.srw.billcalculator;
 import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.text.Html;
-import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
@@ -18,14 +18,48 @@ import java.util.Map;
  */
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    public static final int[] G12_DZIENNA_PREFERENCES = {
+            R.string.preferences_za_energie_czynna_G12dzien,
+            R.string.preferences_skladnik_jakosciowy_G12dzien,
+            R.string.preferences_oplata_sieciowa_G12dzien
+    };
+
+    public static final int[] G12_NOCNA_PREFERENCES = {
+            R.string.preferences_za_energie_czynna_G12noc,
+            R.string.preferences_skladnik_jakosciowy_G12noc,
+            R.string.preferences_oplata_sieciowa_G12noc
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+        addPrefixForG12();
+        changePreferenceVisibilityDependingOnTaryfa();
         setWspKonwersjiDescription();
         setSummary();
+    }
+
+    private void addPrefixForG12() {
+        for (int prefKeyId : G12_DZIENNA_PREFERENCES) {
+            findPreference(getString(prefKeyId)).setTitle(findPreference(getString(prefKeyId)).getTitle() + getString(R.string.postfix_strefa_dzienna));
+        }
+        for (int prefKeyId : G12_NOCNA_PREFERENCES) {
+            findPreference(getString(prefKeyId)).setTitle(findPreference(getString(prefKeyId)).getTitle() + getString(R.string.postfix_strefa_nocna));
+        }
+    }
+
+    private void changePreferenceVisibilityDependingOnTaryfa() {
+        CheckBoxPreference taryfaPreferece = (CheckBoxPreference) findPreference(getString(R.string.preferences_taryfa_dwustrefowa));
+        if (taryfaPreferece.isChecked()) {
+            findPreference(getString(R.string.preferences_G11)).setEnabled(false);
+            findPreference(getString(R.string.preferences_G12)).setEnabled(true);
+        } else {
+            findPreference(getString(R.string.preferences_G11)).setEnabled(true);
+            findPreference(getString(R.string.preferences_G12)).setEnabled(false);
+        }
     }
 
     private void setWspKonwersjiDescription() {
@@ -46,8 +80,7 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
         Map<String, ?> preferences = sharedPreferences.getAll();
         for (String key : preferences.keySet()) {
-            EditTextPreference preference = (EditTextPreference) findPreference(key);
-            preference.setSummary(sharedPreferences.getString(key, "0.0"));
+            updateSummary(sharedPreferences, key);
         }
     }
 
@@ -66,7 +99,20 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        EditTextPreference preference = (EditTextPreference) findPreference(key);
-        preference.setSummary(sharedPreferences.getString(key, "0.0"));
+        updateSummary(sharedPreferences, key);
+        changeTaryfa(sharedPreferences, key);
+    }
+
+    private void updateSummary(final SharedPreferences sharedPreferences, final String key) {
+        Preference preference = findPreference(key);
+        if (preference instanceof EditTextPreference) {
+            preference.setSummary(sharedPreferences.getString(key, "0.0"));
+        }
+    }
+
+    private void changeTaryfa(final SharedPreferences sharedPreferences, final String key) {
+        if (key.equals(getString(R.string.preferences_taryfa_dwustrefowa))) {
+            changePreferenceVisibilityDependingOnTaryfa();
+        }
     }
 }
