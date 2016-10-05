6b8fbf02f1fff882db1aa0f2f269a145dc2d44e9
Kamil Seweryn <sewerk@gmail.com>
2016-10-05 15:15:45 +0200

    to manual Parcelable

--- app/src/main/java/pl/srw/billcalculator/history/list/SelectedBill.java
+++ app/src/main/java/pl/srw/billcalculator/history/list/SelectedBill.java
@@ -1,13 +1,13 @@
 package pl.srw.billcalculator.history.list;
 
-import hrisey.Parcelable;
+import android.os.Parcel;
+
 import pl.srw.billcalculator.db.Bill;
 import pl.srw.billcalculator.persistence.type.BillType;
 
 /**
  * Created by Kamil Seweryn.
  */
-@Parcelable
 public class SelectedBill implements android.os.Parcelable {
 
     private final BillType type;
@@ -31,4 +31,34 @@ public class SelectedBill implements android.os.Parcelable {
     public Long getPricesId() {
         return pricesId;
     }
+
+    protected SelectedBill(Parcel in) {
+        type = BillType.values()[in.readInt()];
+        billId = in.readLong();
+        pricesId = in.readLong();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(type.ordinal());
+        dest.writeLong(billId);
+        dest.writeLong(pricesId);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0; // TODO: check if 0 is ok
+    }
+
+    public static final Creator<SelectedBill> CREATOR = new Creator<SelectedBill>() {
+        @Override
+        public SelectedBill createFromParcel(Parcel in) {
+            return new SelectedBill(in);
+        }
+
+        @Override
+        public SelectedBill[] newArray(int size) {
+            return new SelectedBill[size];
+        }
+    };
 }
--- app/src/main/java/pl/srw/billcalculator/util/MultiSelect.java
+++ app/src/main/java/pl/srw/billcalculator/util/MultiSelect.java
@@ -1,36 +1,38 @@
 package pl.srw.billcalculator.util;
 
-import android.support.v4.util.ArrayMap;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.SparseArray;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 import hugo.weaving.DebugLog;
 
 @lombok.ToString(includeFieldNames = true)
-public class MultiSelect<P, I> {
+public class MultiSelect<I extends Parcelable> implements Parcelable {
 
-    private Map<P, I> selectedItems;
+    private static final String BUNDLE_KEY = "value";
+    private SparseArray<I> selectedItems;
 
     public MultiSelect() {
-        selectedItems = new ArrayMap<>();
+        selectedItems = new SparseArray<>();
     }
 
     @DebugLog
-    public void select(final P p, final I o) {
+    public void select(final int p, final I o) {
         selectedItems.put(p, o);
     }
 
     @DebugLog
-    public void deselect(final P p) {
+    public void deselect(final int p) {
         selectedItems.remove(p);
     }
 
-    public boolean isSelected(final P p) {
-        return selectedItems.containsKey(p);
+    public boolean isSelected(final int p) {
+        return selectedItems.indexOfKey(p) >= 0;
     }
 
     public boolean isAnySelected() {
@@ -42,12 +44,47 @@ public class MultiSelect<P, I> {
     }
 
     public Collection<I> getItems() {
-        return selectedItems.values();
+        final ArrayList<I> values = new ArrayList<>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            values.add(selectedItems.valueAt(i));
+        }
+        return values;
     }
 
-    public Collection<P> getPositionsReverseOrder() {
-        final List<P> keys = new LinkedList<>(selectedItems.keySet());
-        Collections.sort(keys, Collections.reverseOrder());
-        return keys;
+    public int[] getPositionsReverseOrder() {
+        int[] keys = new int[selectedItems.size()];
+        for (int i = 0; i < selectedItems.size(); i++) {
+            keys[i] = selectedItems.keyAt(i);
+        }
+        Arrays.sort(keys);
+        return Sorter.revert(keys);
     }
+
+    protected MultiSelect(Parcel in) {
+        selectedItems = in.readBundle(getClass().getClassLoader()).getSparseParcelableArray(BUNDLE_KEY);
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        final Bundle bundle = new Bundle();
+        bundle.putSparseParcelableArray(BUNDLE_KEY, selectedItems);
+        dest.writeBundle(bundle);
+    }
+
+    @Override
+    public int describeContents() {
+        return 1;// TODO: check if 0 is ok
+    }
+
+    public static final Creator<MultiSelect> CREATOR = new Creator<MultiSelect>() {
+        @Override
+        public MultiSelect createFromParcel(Parcel in) {
+            return new MultiSelect(in);
+        }
+
+        @Override
+        public MultiSelect[] newArray(int size) {
+            return new MultiSelect[size];
+        }
+    };
 }
