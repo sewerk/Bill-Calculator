70244c6049789ea4146afc3315a5da93448bd5a1
Kamil Seweryn <sewerk@gmail.com>
2016-10-17 23:13:44 +0200

    removing negative button from OZE dialog

--- app/src/main/java/pl/srw/billcalculator/dialog/BillCalculatedBeforeOZEChangeDialogFragment.java
+++ app/src/main/java/pl/srw/billcalculator/dialog/BillCalculatedBeforeOZEChangeDialogFragment.java
@@ -3,12 +3,9 @@ package pl.srw.billcalculator.dialog;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
 import pl.srw.billcalculator.R;
-import pl.srw.billcalculator.history.HistoryActivity;
-
 
 public class BillCalculatedBeforeOZEChangeDialogFragment extends DialogFragment {
 
@@ -17,18 +14,7 @@ public class BillCalculatedBeforeOZEChangeDialogFragment extends DialogFragment 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(getString(R.string.dialog_bill_without_oze_title))
                 .setMessage(getString(R.string.dialog_bill_without_oze_message))
-                .setPositiveButton(R.string.dialog_bill_without_oze_positive, onPositiveButton())
-                .setNegativeButton(R.string.dialog_bill_without_oze_cancel, null);
+                .setPositiveButton(R.string.dialog_bill_without_oze_positive, null);
         return builder.create();
-    }
-
-    private DialogInterface.OnClickListener onPositiveButton() {
-        return new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-//                getActivity().setResult(HistoryActivity.REQUEST_RESULT_RECALCULATE_BILL);
-//                getActivity().finish();
-            }
-        };
     }
 }
