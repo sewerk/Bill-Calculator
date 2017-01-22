af1e9b9d9ffd5b4bcf2edfa85d78da96ef766578
Kamil Seweryn <sewerk@gmail.com>
2017-01-22 13:33:35 +0100

    Add LeakCanary

--- app/src/main/java/pl/srw/billcalculator/BillCalculator.java
+++ app/src/main/java/pl/srw/billcalculator/BillCalculator.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
+import com.squareup.leakcanary.LeakCanary;
 
 import javax.inject.Inject;
 
@@ -30,8 +31,16 @@ public class BillCalculator extends Application {
     public void onCreate() {
         super.onCreate();
 
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+            Database.enableDatabaseLogging();
         }
 
         applicationComponent = DaggerApplicationComponent.builder()
