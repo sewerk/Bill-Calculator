bbe27b6179ea3fd37845f785a04e8ff48d5c42a4
Kamil Seweryn <sewerk@gmail.com>
2018-01-15 21:36:03 +0100

    Fix detekt errors

--- app/src/main/java/pl/srw/billcalculator/data/settings/prices/EnergyTariff.kt
+++ app/src/main/java/pl/srw/billcalculator/data/settings/prices/EnergyTariff.kt
@@ -3,6 +3,7 @@ package pl.srw.billcalculator.data.settings.prices
 import android.support.annotation.StringRes
 import pl.srw.billcalculator.R
 
+@SuppressWarnings("EnumNaming")
 enum class EnergyTariff(@StringRes val summaryRes: Int) {
     G11(R.string.settings_energy_tariff_pick_G11),
     G12(R.string.settings_energy_tariff_pick_G11)
