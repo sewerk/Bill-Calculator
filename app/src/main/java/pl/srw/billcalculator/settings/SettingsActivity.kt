e6f80a0b7e2ba8d008698c858f066bc347a72089
Kamil Seweryn <sewerk@gmail.com>
2018-02-11 14:44:36 +0100

    Link screens with new settings

--- app/src/main/java/pl/srw/billcalculator/settings/SettingsActivity.kt
+++ app/src/main/java/pl/srw/billcalculator/settings/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package pl.srw.billcalculator.settings
 
+import android.content.Context
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.bluelinelabs.conductor.Conductor
@@ -9,19 +11,42 @@ import pl.srw.billcalculator.R
 import pl.srw.billcalculator.common.BackableActivity
 import pl.srw.billcalculator.databinding.SettingsActivityBinding
 import pl.srw.billcalculator.di.Dependencies
+import pl.srw.billcalculator.settings.details.SettingsDetailsController
 import pl.srw.billcalculator.settings.list.SettingsController
+import pl.srw.billcalculator.type.Provider
 import timber.log.Timber
 
+private const val ARG_PROVIDER = "settings.provider"
+
 class SettingsActivity : BackableActivity() {
+
+    companion object {
+        @JvmStatic fun start(context: Context) {
+            context.startActivity(Intent(context, SettingsActivity::class.java))
+        }
+
+        @JvmStatic fun start(context: Context, provider: Provider) {
+            val intent = Intent(context, SettingsActivity::class.java)
+            intent.putExtra(ARG_PROVIDER, provider)
+            context.startActivity(intent)
+        }
+    }
 
     private lateinit var router: Router
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<SettingsActivityBinding>(this, R.layout.settings_activity)
+        val provider = intent.getSerializableExtra(ARG_PROVIDER) as Provider?
 
         router = Conductor.attachRouter(this, binding.container, savedInstanceState)
-        if (!router.hasRootController()) router.setRoot(RouterTransaction.with(SettingsController()))
+        if (!router.hasRootController()) {
+            val controller = if (provider != null)
+                SettingsDetailsController.createFor(provider)
+            else
+                SettingsController()
+            router.setRoot(RouterTransaction.with(controller))
+        }
     }
 
     override fun onDestroy() {
