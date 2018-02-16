cfa816b1a3ca7e9d0933846d04cb5fb13ce69de5
Kamil Seweryn <sewerk@gmail.com>
2018-02-17 00:10:35 +0100

    Settings links open browser

--- app/src/main/java/pl/srw/billcalculator/settings/details/dialog/InputSettingsDialogFragment.kt
+++ app/src/main/java/pl/srw/billcalculator/settings/details/dialog/InputSettingsDialogFragment.kt
@@ -6,7 +6,11 @@ import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AlertDialog
+import android.text.Html
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
+import android.widget.TextView
 import pl.srw.billcalculator.R
 import pl.srw.billcalculator.common.bundleOf
 import pl.srw.billcalculator.databinding.SettingsInputDialogBinding
@@ -17,6 +21,7 @@ import pl.srw.billcalculator.settings.details.SettingsDetailsVMFactory
 import javax.inject.Inject
 
 private const val ARG_DATA = "Settings.details.dialog.input.data"
+private const val HTML_PATTERN_TO_HANDLE = "href"
 
 class InputSettingsDialogFragment : DialogFragment() {
 
@@ -33,7 +38,7 @@ class InputSettingsDialogFragment : DialogFragment() {
     @Inject lateinit var vmFactory: SettingsDetailsVMFactory
 
     private val data by lazy { arguments!!.getParcelable(ARG_DATA) as InputSettingsDetailsListItem }
-    private val vm by lazy { ViewModelProviders.of(activity!!, vmFactory).get(SettingsDetailsVM::class.java)}
+    private val vm by lazy { ViewModelProviders.of(activity!!, vmFactory).get(SettingsDetailsVM::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,12 +52,39 @@ class InputSettingsDialogFragment : DialogFragment() {
             this.measureId = data.measure
         }
 
-        return AlertDialog.Builder(context!!)
-                .setTitle(data.title)
-                .setMessage(data.description?: R.string.empty)
-                .setView(binding.root)
-                .setPositiveButton(R.string.settings_input_accept, { _, _ -> vm.valueChanged(data.title, binding.value!!) })
-                .setNegativeButton(R.string.settings_input_cancel, { _, _ -> dismiss() })
-                .create()
+        val description = getStringDescription(data.description)
+        return buildDialog(description, binding)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // setting movement link clickable need to happen after show()
+        makeMessageClickable(dialog)
+    }
+
+    private fun buildDialog(description: CharSequence?,
+                            binding: SettingsInputDialogBinding) =
+        AlertDialog.Builder(context!!)
+            .setTitle(data.title)
+            .setMessage(description)
+            .setView(binding.root)
+            .setPositiveButton(R.string.settings_input_accept, { _, _ -> vm.valueChanged(data.title, binding.value!!) })
+            .setNegativeButton(R.string.settings_input_cancel, { _, _ -> dismiss() })
+            .create()
+
+    private fun getStringDescription(resId: Int?): CharSequence? {
+        val description = resId?.let { getString(it) }
+        return convertFromHtmlIfNecessary(description).first
+    }
+
+    private fun convertFromHtmlIfNecessary(description: String?): Pair<CharSequence?, Boolean> =
+        if (description != null && description.contains(HTML_PATTERN_TO_HANDLE)) {
+            @Suppress("DEPRECATION")
+            Html.fromHtml(description) to true
+        } else description to false
+
+    private fun makeMessageClickable(dialog: Dialog) {
+        val textView = dialog.findViewById<TextView>(android.R.id.message)
+        if (textView?.text is Spanned) textView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
